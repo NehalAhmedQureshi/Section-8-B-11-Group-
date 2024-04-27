eb25c334639ac7145f56a8ea9b1db9b723541342
Nehal Ahmed Qureshi <m.nehalq125@gmail.com>
2024-04-28 00:16:20 +0500

    01 mini assignment done

---
+++ 01_miniAssignment.js
@@ -0,0 +1,21 @@
+
+let array = [
+      ['Fruits', ['Apple', 'Banana', 'Mango']],
+      ['Vegitables', ['Potato', 'Carrot', 'Chilli']],
+      ['Dryfruits', 'Refreshments', ['Pista', 'Kaju', 'peanut', ['Juice', 'COld drinks', 'icecream']]]
+]
+
+// -------------------------- Q No - 1 ------------------------
+console.log(array[1][1][1]);
+
+// -------------------------- Q No - 2 ------------------------
+console.log(array[1][1][2]);
+
+// -------------------------- Q No - 3 ------------------------
+console.log(array[2][2][2]);
+
+
+// -------------------------- Q No - 4 ------------------------
+console.log(array[2][2][3]);
+
+//----------------------------- Complete ----------------------
